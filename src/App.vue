9679650138e49bfdc9b65e01d259df5bc813b0b5
kebwlmbhee <yhqchiu@gmail.com>
2023-04-09 15:12:57 +0800

    feat: add base chatroom

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,135 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="chat-container">
+    <div class="messages-container">
+      <div class="message" v-for="(message, index) in messages" :key="index">
+        <div class="message-info">
+          <div class="message-author">{{ message.author }}</div>
+          <div class="message-time">{{ message.time }}</div>
+        </div>
+        <div class="message-text">{{ message.text }}</div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="form-container">
+    <form class="message-input" @submit.prevent="sendMessage">
+      <input type="text" v-model="author" placeholder="Your name">
+      <input type="text" v-model="text" placeholder="Type your message here...">
+      <button type="submit">Send</button>
+    </form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import { ref, push, onValue } from 'firebase/database';
+import { defineComponent } from 'vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default defineComponent({
+  data() {
+    return {
+      author: '',
+      text: '',
+      messages: []
+    }
+  },
+  created() {
+    const chatroomRef = ref(this.$db, 'chatroom');
+    onValue(chatroomRef, (snapshot) => {
+      const messages = [];
+      snapshot.forEach((childSnapshot) => {
+        messages.push(childSnapshot.val());
+      });
+      this.messages = messages;
+    });
+  },
+  methods: {
+    sendMessage() {
+      if (!this.author) {
+      alert('Please enter your name');
+      return;
+    }
+      const newMessage = {
+        author: this.author,
+        text: this.text,
+        time: new Date().toLocaleString()
+      }
+      push(ref(this.$db, 'chatroom'), newMessage);
+      this.text = '';
+    }
   }
+});
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
+.messages-container {
+  flex: 1;
+  overflow-y: scroll;
+}
+.message {
+  font-size: 30px;
+  margin: auto;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+.message-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+}
+.message-author {
+  font-weight: bold;
+}
+.message-time {
+  margin-top: auto; 
+  font-size: 20px;
+  color: #666;
+}
+.message-text {
+  white-space: pre-wrap;
+}
+.message-input {
+  display: flex;
+  align-items: center;
+  /* margin: auto; */
+  margin-top: 30%;
+  padding: 10px;
+  border-top: 1px solid #ccc;
+}
+.message-input input {
+  width: 150%;
+  height: 40px;
+  font-size: 1.3rem;
+  margin-right: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+}
+.message-input button {
+  width: 10%;
+  height: 45px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #4CAF50;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+input::placeholder{
+  font-size: 20px;
+}
+
+.form-container {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,44 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
 
-import './assets/main.css'
 
-const app = createApp(App)
+// Firebase configuration
+// Firebase 設定
+const firebaseConfig = {
+    apiKey: 'AIzaSyCnoF3pVGktFkrQ1vn6U6k-g8zMifrsFL4',
+    authDomain: 'memusic-97d9b.firebaseapp.com',
+    databaseURL: 'https://memusic-97d9b-default-rtdb.firebaseio.com',
+    projectId: 'memusic-97d9b',
+    storageBucket: 'memusic-97d9b.appspot.com',
+    messagingSenderId: '285609111713',
+    appId: '1:285609111713:web:77cb667268d9d2f9b880e0',
+    measurementId: 'G-G9HCVQN5R9'
+}
 
-app.use(createPinia())
-app.use(router)
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
 
-app.mount('#app')
+// Get Firebase Realtime Database instance
+const db = getDatabase(app);
+
+// Create Vue app instance
+const vueApp = createApp(App);
+
+// Inject Firebase Realtime Database instance as $db
+vueApp.config.globalProperties.$db = db;
+
+// Mount Vue app
+vueApp.mount('#app');
+
+// Define component for listening to database changes
+export default {
+    created() {
+      const chatroomRef = ref(this.$db, 'chatroom');
+      onValue(chatroomRef, (snapshot) => {
+        console.log(snapshot.val());
+      });
+    }
+  }
