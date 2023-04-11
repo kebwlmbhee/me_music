9557f7a9c3674c723217480725961f09c7dc8d11
zero300 <qoo900806@gmail.com>
2023-04-11 16:55:56 +0800

    refactor
    修改一些程式, 調整其位置

--- src/main.js
+++ src/main.js
@@ -6,25 +6,17 @@ import router from './router'
 
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 // add Vuetify icon
 import iconFonts from '@/plugins/vuetify'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
 
 // 會影響 Vuetify
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
 app.use(iconFonts)
 
 app.mount('#app')
