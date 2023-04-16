f09adb62cf355a1ad60197c7f317ce72aa2beea2
kebwlmbhee <yhqchiu@gmail.com>
2023-04-16 22:25:17 +0800

    Merge 'dev' into 'create_base_chatroom'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# spotify-partyapp
+# me_music
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/me_music/",   // Deal with the path problem when deploying to GitLab or GitHub
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
