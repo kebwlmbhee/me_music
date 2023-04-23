8e53b97a5f72f4edf8ac0eb85d84c043673d2ece
zhi-rong <jordan990301@gmail.com>
2023-04-23 20:17:34 +0800

    Fix bugs but still exists Vue resolution warning on vutify components while Vitest

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            return tag.startsWith('v-') // (return true), 避免 Vitest 跳警告 v-xxx 無法 resolve
-          }
-        }
-      }
-    }),
+    vue(),
   ],
   // base: "/me_music/",   // Deal with the path problem when deploying to GitLab or GitHub
   resolve: {
