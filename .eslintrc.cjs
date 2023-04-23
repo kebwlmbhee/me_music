06b356e23b82b9e56c587301f6cd4e0b070a6f1c
zhi-rong <jordan990301@gmail.com>
2023-04-23 17:24:40 +0800

    fix some bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting',
-    //
+    // 避免 eslint 阻擋
     'plugin:cypress/recommended'
   ],
   parserOptions: {
--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 
   component: {
