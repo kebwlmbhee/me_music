03ceb7d5361741284acee6ea2485aa5b37f0308b
kebwlmbhee <yhqchiu@gmail.com>
2023-05-01 23:07:06 +0800

    complete chatroom comments

--- src/views/chatroom/chatroom.js
+++ src/views/chatroom/chatroom.js
@@ -6,7 +6,9 @@ class Chatroom {
     this.announcementRef = ref(db, 'announcement')
   }
 
+  // 傳送聊天室訊息
   sendMessage(author, text, isAnnounce) {
+    // 這裡設置的屬性會傳入 firebase，所以將參數給定 newMessage 的屬性值
     const newMessage = {
       author: author,
       text: text,
@@ -14,6 +16,7 @@ class Chatroom {
       isAnnounce: isAnnounce
     }
     push(this.chatroomRef, newMessage)
+    // 如果 isAnnounce(checkbox)為 true，則寫入公告
     if (isAnnounce) {
       const announcement = push(this.announcementRef)
       const announcementId = announcement.key
@@ -23,6 +26,7 @@ class Chatroom {
     }
   }
 
+  // 偵測聊天室訊息變動，如果有變動，實時更新
   onMessage(callback) {
     onValue(this.chatroomRef, (snapshot) => {
       const messages = []
@@ -35,6 +39,7 @@ class Chatroom {
     })
   }
 
+  // 偵測公告變動，如果有變動，實時更新
   onAnnouncement(callback) {
     onValue(this.announcementRef, (snapshot) => {
       const messages = []
@@ -47,12 +52,14 @@ class Chatroom {
     })
   }
 
+  // 移除多餘的公告，因為上限 10 筆，如果超過 10 筆，就要調用此 function 移除最舊的公告
   removeAnnouncement(announcement) {
     console.log(announcement)
     console.log(announcement.id)
     remove(ref(db, `/announcement/${announcement.id}`))
   }
 
+  // 將時戳根據電腦的時區換算顯示時間
   getTimeString(timestamp) {
     const date = new Date(timestamp)
     const hour = date.getHours()
