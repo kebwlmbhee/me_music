f965624a481b7103a756b04e6da0db40fb07b499
kebwlmbhee <yhqchiu@gmail.com>
2023-05-09 12:31:44 +0800

    fix: delete useless beforeEach import from the vitest

--- __test__/views/chatroom/ChatView.test.js
+++ __test__/views/chatroom/ChatView.test.js
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import vuetify from "@/plugins/vuetify";
 import router from "@/router";
