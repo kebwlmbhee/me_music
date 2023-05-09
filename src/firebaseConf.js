d568dbe5b4cdd2fb96d9229eaa98a15b781b7278
kebwlmbhee <yhqchiu@gmail.com>
2023-05-10 03:06:57 +0800

    fix: firebase security vulnerability

--- src/firebaseConf.js
+++ src/firebaseConf.js
@@ -12,21 +12,11 @@ import {
   get
 } from 'firebase/database'
 import { getAuth } from 'firebase/auth'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCnoF3pVGktFkrQ1vn6U6k-g8zMifrsFL4',
-  authDomain: 'memusic-97d9b.firebaseapp.com',
-  databaseURL: 'https://memusic-97d9b-default-rtdb.firebaseio.com',
-  projectId: 'memusic-97d9b',
-  storageBucket: 'memusic-97d9b.appspot.com',
-  messagingSenderId: '285609111713',
-  appId: '1:285609111713:web:77cb667268d9d2f9b880e0',
-  measurementId: 'G-G9HCVQN5R9'
-}
+import firebaseConfig from '/.firebase/firebaseInfo.js'
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-let db = getDatabase(app)
+const db = getDatabase(app)
 
 export {
   firebaseConfig,
