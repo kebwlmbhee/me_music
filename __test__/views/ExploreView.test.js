f37bbf90be5110d24a921a6af024b32b1d79bcf8
zhi-rong <jordan990301@gmail.com>
2023-05-17 14:30:27 +0800

    add runSearch test

--- __test__/views/ExploreView.test.js
+++ __test__/views/ExploreView.test.js
@@ -1,19 +1,19 @@
 // Test written by jordan990301
 import { describe, expect, it, vi } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import vuetify from "@/plugins/vuetify";
 import router from "@/router";
 import { createTestingPinia } from '@pinia/testing'
 
 import ExploreView from "@/views/ExploreView.vue";
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter'
+import MockAdapter from 'axios-mock-adapter' // https://github.com/ctimmerm/axios-mock-adapter
 
 describe("Test ExploreView.vue", () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallowMount(ExploreView, {
+        wrapper = mount(ExploreView, {
             global: {
                 plugins: [vuetify, router, createTestingPinia()],
                 provide: {
@@ -22,8 +22,12 @@ describe("Test ExploreView.vue", () => {
             },
             data() {
                 return {
+                    loaded: false,
+                    imgsrc: '',
                     playlists: [],
+                    shows: [],
                     artists: [],
+                    search: ''
                 }
             },
         });
@@ -31,6 +35,24 @@ describe("Test ExploreView.vue", () => {
         it('wrapper creation', () => {
             expect(wrapper.exists()).toBe(true);
         })
+    })
+
+    it('runSearch()', async () => {
+        // Mock the searchPlayList and searchArtist methods
+        wrapper.vm.searchPlayList = vi.fn(() => Promise.resolve());
+        wrapper.vm.searchArtist = vi.fn(() => Promise.resolve());
+
+        // Call the runSearch method
+        await wrapper.vm.runSearch();
+        await wrapper.vm.$nextTick();
+
+        // Verify that the loaded state is initially true
+        await expect(wrapper.vm.loaded).toBe(true);
+        // Wait for the Promises to resolve
+        await Promise.all([wrapper.vm.searchPlayList(), wrapper.vm.searchArtist()]);
+
+        // Verify that the loaded state is updated to false
+        await expect(wrapper.vm.loaded).toBe(false);
     })
 
     it('searchPlayList()', async () => {
@@ -89,8 +111,71 @@ describe("Test ExploreView.vue", () => {
 
         await wrapper.vm.searchPlayList()
         await wrapper.vm.$nextTick();
-        console.log("wrapper.vm.playlists")
-        console.log(wrapper.vm.playlists)
+        // console.log("wrapper.vm.playlists")
+        // console.log(wrapper.vm.playlists)
+
+        expect(wrapper.vm.playlists).toEqual((mock_data["playlists"])["items"])
+
+        mock.restore();
+    });
+
+    it('searchArtist()', async () => {
+        let mock = new MockAdapter(axios);
+        let url = "https://api.spotify.com/v1/search?query=c&type=artist&offset=0&limit=10";
+        let mock_data = {
+            "artists": {
+                "href": "https://api.spotify.com/v1/search?query=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=artist&locale=zh-TW%2Czh%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7%2Cja%3Bq%3D0.6%2Ceo%3Bq%3D0.5&offset=0&limit=1",
+                "items": [
+                    {
+                        "external_urls": {
+                            "spotify": "https://open.spotify.com/artist/38PzLQE4GW8o7A18oGhi0x"
+                        },
+                        "followers": {
+                            "href": null,
+                            "total": 9297
+                        },
+                        "genres": [],
+                        "href": "https://api.spotify.com/v1/artists/38PzLQE4GW8o7A18oGhi0x",
+                        "id": "38PzLQE4GW8o7A18oGhi0x",
+                        "images": [
+                            {
+                                "height": 640,
+                                "url": "https://i.scdn.co/image/ab6761610000e5ebb6a7d269c36ebe8eaa505969",
+                                "width": 640
+                            },
+                            {
+                                "height": 320,
+                                "url": "https://i.scdn.co/image/ab67616100005174b6a7d269c36ebe8eaa505969",
+                                "width": 320
+                            },
+                            {
+                                "height": 160,
+                                "url": "https://i.scdn.co/image/ab6761610000f178b6a7d269c36ebe8eaa505969",
+                                "width": 160
+                            }
+                        ],
+                        "name": "dazy",
+                        "popularity": 70,
+                        "type": "artist",
+                        "uri": "spotify:artist:38PzLQE4GW8o7A18oGhi0x"
+                    }
+                ],
+                "limit": 1,
+                "next": "https://api.spotify.com/v1/search?query=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=artist&locale=zh-TW%2Czh%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7%2Cja%3Bq%3D0.6%2Ceo%3Bq%3D0.5&offset=1&limit=1",
+                "offset": 0,
+                "previous": null,
+                "total": 800
+            }
+        }
+
+        mock.onGet(url).reply(200, mock_data);
+
+        await wrapper.vm.searchArtist()
+        await wrapper.vm.$nextTick();
+        // console.log("wrapper.vm.artists")
+        // console.log(wrapper.vm.artists)
+
+        expect(wrapper.vm.artists).toEqual((mock_data["artists"])["items"])
 
         mock.restore();
     });
