2917c9682af766d97980710f570c3ae6964eccc9
zhi-rong <jordan990301@gmail.com>
2023-05-17 13:50:12 +0800

    axios searchPlayList

--- __test__/views/ExploreView.test.js
+++ __test__/views/ExploreView.test.js
@@ -7,10 +7,7 @@ import { createTestingPinia } from '@pinia/testing'
 
 import ExploreView from "@/views/ExploreView.vue";
 import axios from 'axios';
-
-// https://vitest.dev/api/vi.html#vi-mock
-vi.mock('axios')
-vi.mock("@/views/ExploreView.vue")
+import MockAdapter from 'axios-mock-adapter'
 
 describe("Test ExploreView.vue", () => {
     let wrapper;
@@ -25,109 +22,76 @@ describe("Test ExploreView.vue", () => {
             },
             data() {
                 return {
-                    playlists: {
-                        items: []
-                    },
-                    artists: {
-                        items: []
-                    }
+                    playlists: [],
+                    artists: [],
                 }
             },
         });
 
-        axios.mockResolvedValue({
-            data: {
-                playlists: {
-                    items: [
-                        {
-                            "collaborative": false,
-                            "description": "Enjoy a break with some relaxing soft songs.",
-                            "external_urls": {
-                                "spotify": "https://open.spotify.com/playlist/37i9dQZF1DXcxacyAXkQDu"
-                            },
-                            "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXcxacyAXkQDu",
-                            "id": "37i9dQZF1DXcxacyAXkQDu",
-                            "images": [
-                                {
-                                    "height": null,
-                                    "url": "https://i.scdn.co/image/ab67706f000000032882fd7da8b1d327d94d98a4",
-                                    "width": null
-                                }
-                            ],
-                            "name": "Cozy Blend",
-                            "owner": {
-                                "display_name": "Spotify",
-                                "external_urls": {
-                                    "spotify": "https://open.spotify.com/user/spotify"
-                                },
-                                "href": "https://api.spotify.com/v1/users/spotify",
-                                "id": "spotify",
-                                "type": "user",
-                                "uri": "spotify:user:spotify"
-                            },
-                            "primary_color": null,
-                            "public": null,
-                            "snapshot_id": "MTY4NDIyNzU5NiwwMDAwMDAwMDExODIxMzA4ODg3Y2Q0OWNiMjlhZGZkM2M1NzliNDg3",
-                            "tracks": {
-                                "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXcxacyAXkQDu/tracks",
-                                "total": 250
-                            },
-                            "type": "playlist",
-                            "uri": "spotify:playlist:37i9dQZF1DXcxacyAXkQDu"
-                        }
-                    ]
-                },
-                artists: {
-                    items: [
-                        {
-                            "external_urls": {
-                                "spotify": "https://open.spotify.com/artist/38PzLQE4GW8o7A18oGhi0x"
-                            },
-                            "followers": {
-                                "href": null,
-                                "total": 9290
-                            },
-                            "genres": [],
-                            "href": "https://api.spotify.com/v1/artists/38PzLQE4GW8o7A18oGhi0x",
-                            "id": "38PzLQE4GW8o7A18oGhi0x",
-                            "images": [
-                                {
-                                    "height": 640,
-                                    "url": "https://i.scdn.co/image/ab6761610000e5ebb6a7d269c36ebe8eaa505969",
-                                    "width": 640
-                                },
-                                {
-                                    "height": 320,
-                                    "url": "https://i.scdn.co/image/ab67616100005174b6a7d269c36ebe8eaa505969",
-                                    "width": 320
-                                },
-                                {
-                                    "height": 160,
-                                    "url": "https://i.scdn.co/image/ab6761610000f178b6a7d269c36ebe8eaa505969",
-                                    "width": 160
-                                }
-                            ],
-                            "name": "dazy",
-                            "popularity": 70,
-                            "type": "artist",
-                            "uri": "spotify:artist:38PzLQE4GW8o7A18oGhi0x"
-                        }
-                    ]
-                }
-            }
+        it('wrapper creation', () => {
+            expect(wrapper.exists()).toBe(true);
         })
     })
 
-    // https://stackblitz.com/edit/vitest-dev-vitest-aqctgw?file=test%2Ffactory.test.ts&initialPath=__vitest__/
-    test('axios is mocked', () => {
-        expect(vi.isMockFunction(axios.get)).toBe(true)
-        expect(vi.isMockFunction(axios)).toBe(true)
-    })
+    it('searchPlayList()', async () => {
+        let mock = new MockAdapter(axios);
+        let url = "https://api.spotify.com/v1/search?query=t&type=playlist&limit=20"
+        let mock_data = {
+            "playlists": {
+                "href": "https://api.spotify.com/v1/search?query=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=playlist&locale=zh-TW%2Czh%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7%2Cja%3Bq%3D0.6%2Ceo%3Bq%3D0.5&offset=0&limit=1",
+                "items": [
+                    {
+                        "collaborative": false,
+                        "description": "Enjoy a break with some relaxing soft songs.",
+                        "external_urls": {
+                            "spotify": "https://open.spotify.com/playlist/37i9dQZF1DXcxacyAXkQDu"
+                        },
+                        "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXcxacyAXkQDu",
+                        "id": "37i9dQZF1DXcxacyAXkQDu",
+                        "images": [
+                            {
+                                "height": null,
+                                "url": "https://i.scdn.co/image/ab67706f000000032882fd7da8b1d327d94d98a4",
+                                "width": null
+                            }
+                        ],
+                        "name": "Cozy Blend",
+                        "owner": {
+                            "display_name": "Spotify",
+                            "external_urls": {
+                                "spotify": "https://open.spotify.com/user/spotify"
+                            },
+                            "href": "https://api.spotify.com/v1/users/spotify",
+                            "id": "spotify",
+                            "type": "user",
+                            "uri": "spotify:user:spotify"
+                        },
+                        "primary_color": null,
+                        "public": null,
+                        "snapshot_id": "MTY4NDIyNzU5NiwwMDAwMDAwMDExODIxMzA4ODg3Y2Q0OWNiMjlhZGZkM2M1NzliNDg3",
+                        "tracks": {
+                            "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXcxacyAXkQDu/tracks",
+                            "total": 250
+                        },
+                        "type": "playlist",
+                        "uri": "spotify:playlist:37i9dQZF1DXcxacyAXkQDu"
+                    }
+                ],
+                "limit": 1,
+                "next": "https://api.spotify.com/v1/search?query=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=playlist&locale=zh-TW%2Czh%3Bq%3D0.9%2Cen-US%3Bq%3D0.8%2Cen%3Bq%3D0.7%2Cja%3Bq%3D0.6%2Ceo%3Bq%3D0.5&offset=1&limit=1",
+                "offset": 0,
+                "previous": null,
+                "total": 716
+            }
+        }
 
-    it('should load playlists and artists on creation', async () => {
-        await wrapper.vm.searchArtist()
+        mock.onGet(url).reply(200, mock_data);
+
+        await wrapper.vm.searchPlayList()
+        await wrapper.vm.$nextTick();
+        console.log("wrapper.vm.playlists")
         console.log(wrapper.vm.playlists)
-        // const getTokenSpy = vi.spyOn(wrapper.vm.authCode, 'access_token')
-        // getTokenSpy.mockReturnValue('12345')
+
+        mock.restore();
     });
 })
